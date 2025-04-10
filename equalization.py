3a8117ed3aa3268fddb2243702dc9215a115226b
shivaansh81001 <shivaans@ualberta.ca>
2025-04-09 20:21:37 -0600

    so far so good, there are some outlier images but they are bad images overall anyways

--- equalization.py
+++ equalization.py
@@ -4,20 +4,20 @@ from skimage.util import img_as_ubyte
 import os, glob,pathlib
 import matplotlib.pyplot as plt
 
-def show_result( refr, images):
+def show_result( before, after):
     
     plt.figure(figsize=(10, 12)) 
     images = [
-        (refr, 'Original Image'),
-        (laplacian, '1'),
-        (refr, 'Original Image'),
-        (gaussian, '2'),
-        (refr,'original'),
-        (b_part, '3'),
-        (refr, 'Original Image'),
-        (Q3, '4'),
-        (refr, 'Original Image'),
-        (Q4, '5')
+        (before[0], 'Original Image'),
+        (after[0], '1'),
+        (before[1], 'Original Image'),
+        (after[1], '2'),
+        (before[2],'original'),
+        (after[2], '3'),
+        (before[3], 'Original Image'),
+        (after[3], '4'),
+        (before[4], 'Original Image'),
+        (after[4], '5')
     ]
 
     for i, (img, title) in enumerate(images, start=1):
@@ -34,8 +34,9 @@ def apply_median(image):
 def hist_matching(image,refr):
     matched = np.clip(exposure.match_histograms(image,refr),0,255).astype(np.uint8)
     #matched = apply_median(matched)
+    matched[matched < 20] = 0
     matched = exposure.equalize_hist(matched,mask=matched<255)
-    print(exposure.histogram(matched))
+    #print(exposure.histogram(matched))
 
     return matched
 
@@ -46,14 +47,21 @@ def test():
     REFERENCE_FILENAME = r"Training\notumor\Tr-no_0538.jpg"
     refr_img = img_as_ubyte(io.imread(REFERENCE_FILENAME,as_gray=True))
 
-    images=[]
-    for number in np.random.randint(1594,size=(5)):
-        image = img_as_ubyte(io.imread(f"Training\notumor\Tr-no_{number}.jpg",as_gray=True))
-        matched = hist_matching(image, refr_img)
-        images.append(matched)
+    for i in range(5):
+        before_images=[]
+        after_images=[]
+        random_list = np.random.randint(10,1338,size=(5))
+        print(random_list)
 
-    
-    show_result(refr_img,images)
+        for number in random_list:
+            image = img_as_ubyte(io.imread(rf"Training/notumor/Tr-no_{number:04}.jpg",as_gray=True))
+            image[image< 40] = 0
+            before_images.append(image)
+            matched = hist_matching(image, refr_img)
+            after_images.append(matched)
+
+        
+        show_result(before_images,after_images)
     
 
 if __name__=="__main__":
